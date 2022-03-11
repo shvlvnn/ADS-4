e45f8026641f42b70f8711500da2e829c8981c1c
Anna Shevelyova <99125817+shvlvnn@users.noreply.github.com>
2022-03-11 10:46:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -27,20 +27,27 @@ int countPairs2(int *arr, int len, int value) {
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int a = 0; a < len-1; a++) {
-        int n = value - arr[a];
-        int x = a + 1; int y = len - 1;
-        while (x < y) {
+        int x = a; int y = len;
+        while (x < y - 1) {
             int z = (x + y) / 2;
-            if (arr[z] < n)
-            x = z + 1;
-            else
-            y = z;
-        }
-        if (arr[x] == n) {
-            while (arr[x] == n) {
-                count++;
-                x++;
+            if (arr[a] + arr [z] == value) {
+                count ++;
+                int b = z + 1;
+                while ((arr[a] + arr[b] == value) && (b < y)) {
+                    count++;
+                    b++;           
             }
+                b = z - 1;
+                while ((arr[a] + arr[b] == value) && (b > x)) {
+                    count++;
+                    b--;
+        }
+                break;
+    }
+            if (arr[a] + arr[z] > value)
+                y = z;
+            else
+                x= z;
         }
     }
     return count;
